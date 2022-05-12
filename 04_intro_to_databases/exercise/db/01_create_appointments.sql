3d15cf667244bc247e3d9809496177556e2d4d1c
Dakota L Martinez <dakotaleemusic@gmail.com>
2022-05-12 16:32:18 -0700

    add console file

--- 04_intro_to_databases/exercise/db/01_create_appointments.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE appointments(
-  id INTEGER PRIMARY KEY,
-  time DATETIME,
-  doctor STRING,
-  patient STRING,
-  purpose STRING,
-  notes STRING,
-  canceled BOOLEAN DEFAULT FALSE
-);
