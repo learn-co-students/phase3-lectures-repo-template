5c215a28440a86189e69f05fc0ebf329f5bc295a
Dakota L Martinez <dakotaleemusic@gmail.com>
2022-05-18 14:39:47 -0700

    update config/environment

--- 03_object_oriented_ruby_pt2/config/environment.rb
+++ 03_object_oriented_ruby_pt2/config/environment.rb
@@ -13,6 +13,11 @@ require "colorize"
 require "date"
 require "active_support/core_ext/integer/time"
 require "active_support/core_ext/date/calculations"
-# One of our dependencies that we loaded in the previous expression was the require_all gem. It allows us to require all of the files within a directory. The path we pass to it will be relative to the root path of the project (where the Gemfile is). In our case, we're loading all of the files inside of the lib directory, so if we write code in there (like a method) it will be accessible to us after the require_all below
 
+# When we use `require_all`, the path we pass to it will be 
+# relative to the root path of the project (where the Gemfile 
+# is). In our case, we're loading all of the files inside of 
+# the lib directory, so if we write code in there (like a 
+# method) it will be accessible to us after the require_all
+# below
 require_all "lib"
